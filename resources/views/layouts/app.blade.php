ad179a2c909f0e36b386a980c1b21571733a14f3
azaelsanchez <cem20619@gmail.com>
2020-01-28 15:11:08 +0100

    Generando un include

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,6 +56,12 @@
                         <li class="nav-item">
                         <a class="nav-link" href="">Subir Noticia</a>
                         </li>
+                        <li>
+                            @include('includes.avatar')
+                            </li>
+                            <li>
+                                &nbsp; &nbsp;
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
