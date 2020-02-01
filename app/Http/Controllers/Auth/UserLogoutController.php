20106d59af1470577195289895e3ba03e8510b23
azaelsanchez <cem20619@gmail.com>
2020-02-01 19:19:17 +0100

    Logout terminado

---
+++ app/Http/Controllers/Auth/UserLogoutController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+
+class UserLogoutController extends Controller
+{
+    public function logout (Request $request){
+        $body = $request->input();
+        $userTest = DB::table('users')
+            ->where('id', '=', $body{'id'})
+            ->get();
+        if(count($userTest) ===1){
+           DB::table('users')
+               ->where('id', '=', $body{'id'})
+                ->update(['remember_token'=>null]);
+            return response('Desconectado correctamente');
+        }else {
+            return response('Upps ocurrio un error');
+        }
+    }
+}
