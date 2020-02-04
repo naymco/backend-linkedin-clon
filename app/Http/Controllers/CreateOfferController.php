b3234b4c4bd172697001ae5f5a61f6b0bd5c5283
azaelsanchez <cem20619@gmail.com>
2020-02-04 12:53:21 +0100

    CreateOffer work

---
+++ app/Http/Controllers/CreateOfferController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Company;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+class CreateOfferController extends Controller
+{
+    //
+
+    public function CreateOffer(Request $request){
+
+        $oferta =$request->input();
+        DB::table('offer_works')->insert([
+            ['tittle_offer'=>$oferta{'tittle_offer'},
+                'description'=>$oferta{'description'},
+                'experience_level'=>$oferta{'experience_level'},
+                'company_id'=>$oferta{'company_id'},
+                'category_id'=>$oferta{'category_id'},
+                'province_id'=>$oferta{'province_id'},
+                'created_at'=>$oferta{'created_at'},
+                'updated_at'=>null]
+        ]);
+        // return $oferta{''};
+    }
+}
--- routes/company.php
+++ routes/company.php
@@ -3,14 +3,15 @@
 use Illuminate\Http\Request;
 use App\Http\Controllers\DatosController;
 Route::group(['middleware'=>['cors']], function (){
-    Route::group(['middleware'=>['CompanyToken']], function (){
+    //Route::group(['middleware'=>['CompanyToken']], function (){
 
-    Route::post('/creaoferta', 'CreacionOfertaController@CreaOferta');
+    Route::post('/crearoferta', 'CreateOfferController@CreateOffer');
+    Route::get('/versolicitudes/{id}', 'VerSolicitudesController@VerSolicitudesEmpresas');
 
     //ver perfil de la empresa
     Route::get('/verperfil/{id}', 'VerPerfilController@verPerfilEmpresa');
 
-    });
+   // });
     Route::post('/login', 'Auth\CompanyLoginController@loginCompany');
     //Se deslogea un usuario
     Route::post('/logout', 'Auth\CompanyLogoutController@logout');
